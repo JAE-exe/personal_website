d602f92da3a3230e812760cd35215670804fe6b9
Jayden De Souza <113254213+JAE-exe@users.noreply.github.com>
2023-09-25 17:55:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+const containerEl = document.querySelector(".i-am-a");
+
+const careers = ["Student", "Web Developer", "Tech Enthustiast"];
+
+let careerIndex = 0;
+
+let characterIndex = 0;
+
+updateText();
+
+function updateText() {
+  characterIndex++;
+  containerEl.innerHTML = `
+    <h1>I am ${careers[careerIndex].slice(0, 1) === "I" ? "an" : "a"} ${careers[
+    careerIndex
+  ].slice(0, characterIndex)}</h1>
+    `;
+
+  if (characterIndex === careers[careerIndex].length) {
+    careerIndex++;
+    characterIndex = 0;
+  }
+
+  if (careerIndex === careers.length) {
+    careerIndex = 0;
+  }
+  setTimeout(updateText, 400);
+}
